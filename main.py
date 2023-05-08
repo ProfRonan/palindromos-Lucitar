adf795f2a1734c4e86fec9a1d7b8b5fe1972ca75
Victor Lucio <>
2023-05-08 10:32:59 -0300

    Adicionando o código

--- main.py
+++ main.py
@@ -3,4 +3,8 @@
 
 def is_palindrome(string: str) -> bool:
     """Check if string is palindrome."""
-    return False
+    string = string.replace(" ", "").replace("!", "").lower()
+    for i in range(len(string)):
+        if string[i] != string[len(string) - i - 1]:
+            return False
+    return True
